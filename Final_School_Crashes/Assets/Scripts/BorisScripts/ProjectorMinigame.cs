69b5d303dbb61d3b68b9dabb0d639924c4902084
BorisSydney <borissydney21@gmail.com>
2024-06-13 03:03:14 -0300

    Projector Minigame "Finished"
    Basically finished the projector minigame, it just needs some work when it comes to changing the scenes. Also added my scripts folder and some stuff to my scene!

---
+++ Final_School_Crashes/Assets/Scripts/BorisScripts/ProjectorMinigame.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class ProjectorMinigame : MonoBehaviour
+{
+    public int tapsForProjector;
+    public int score;
+
+    public float gameTime;
+    public float remainingTime;
+
+    public bool startGame;
+
+    public TMP_Text timeText;
+
+    public Slider slider;
+    public Button projectorButton;
+
+    void Start()
+    {
+        projectorButton.onClick.AddListener(OnTap);
+        slider.maxValue = 10;
+        slider.value = 0;
+    }
+
+    void Update()
+    {
+        if (startGame)
+        {
+            remainingTime -= Time.deltaTime;
+            if (remainingTime <= 0)
+            {
+                EndGame();
+            }
+            if (score == tapsForProjector)
+            {
+                NextMinigame();
+            }
+            UpdateUI();
+        }
+    }
+
+    void OnTap()
+    {
+        if (startGame)
+        {
+            score++;
+            UpdateUI();
+        }
+    }
+
+    void NextMinigame()
+    {
+        score = 0;
+        SceneManager.LoadScene("Menu");
+    }
+
+    void EndGame()
+    {
+        startGame = false;
+        slider.value = 0;
+        score = 0;
+        timeText.text = "You Lose";
+        SceneManager.LoadScene("Menu");
+    }
+
+    void UpdateUI()
+    {
+        timeText.text = "Time: " + Mathf.Clamp(remainingTime, 0, gameTime).ToString("F2");
+        slider.value = Mathf.Clamp(score, 0, slider.maxValue);
+    }
+}
