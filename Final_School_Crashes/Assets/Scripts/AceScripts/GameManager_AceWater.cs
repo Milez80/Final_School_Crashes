6c3469b8c1e132982a56bc1a932b36b3f9cf013c
ace-is-rad <163329273+ace-is-rad@users.noreply.github.com>
2024-06-12 20:42:49 -0700

    Beginning of Water Script
    Implementing goal and slider

---
+++ Final_School_Crashes/Assets/Scripts/AceScripts/GameManager_AceWater.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using TMPro;
+
+public class GameManager_AceWater : MonoBehaviour
+{
+    public float currentWater;
+    public float WaterPerS;
+    public float WaterPerClick;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Final_School_Crashes/Assets/Scripts/AceScripts/WaterGoal.cs
+++ Final_School_Crashes/Assets/Scripts/AceScripts/WaterGoal.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UIElements;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WaterGoal : MonoBehaviour
 {
+    public float goal;
+    float prevGoal = 0;
+    public Slider waterBar;
+    public GameManager_AceWater GameManager_AceWater;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,12 @@ public class WaterGoal : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        waterBar.value = (GameManager_AceWater.currentWater - prevGoal) / (goal - prevGoal);
+
+        if (GameManager_AceWater.currentWater >= goal)
+        {
+            prevGoal = goal;
+            goal *= 2;
+        }
     }
 }
