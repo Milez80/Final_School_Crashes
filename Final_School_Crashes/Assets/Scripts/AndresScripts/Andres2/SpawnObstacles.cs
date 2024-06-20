01a5dd0fb9d34be45ec7d848a3442703cab85768
Milez80 <miles-80@hotmail.com>
2024-06-20 01:20:39 -0700

    Andres2update

--- Final_School_Crashes/Assets/Scripts/AndresScripts/Andres2/SpawnObstacles.cs
+++ Final_School_Crashes/Assets/Scripts/AndresScripts/Andres2/SpawnObstacles.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class SpawnObstacles : MonoBehaviour
 {
+
+    public GameObject obstacle;
+    public float maxX;
+    public float minX;
+    public float maxY;
+    public float minY;
+    public float timeBetweenSpawn;
+    private float spawnTime;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Spawn();
+        spawnTime = Time.time + timeBetweenSpawn;
+    }
+
+    void Spawn()
+    {
+        float randomX = Random.Range(minX, maxX);
+        float randomY = Random.Range(minY, maxY);
+
+        Instantiate(obstacle, transform.position + new Vector3(randomX, randomY, 0), transform.rotation);
     }
 }
+
